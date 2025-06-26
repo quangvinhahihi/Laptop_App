05ed564d9ad0e449ac969884abd5870b389fb6ac
quangvinhahihi <vinhnguye98@gmail.com>
2025-06-27 00:43:11 +0700

    Lay danh sach san pham theo danh muc

--- src/components/home-type-products/index.tsx
+++ src/components/home-type-products/index.tsx
@@ -1,27 +1,22 @@
 import React, { useState } from "react";
-
-const dataOptions = [
-  {
-    id: 1,
-    label: "Gaming",
-  },
-  {
-    id: 2,
-    label: "Văn phòng",
-  },
-  {
-    id: 3,
-    label: "Đồ hoạ",
-  },
-  {
-    id: 4,
-    label: "Sinh viên",
-  },
-];
+import { products } from "./fakeData";
+import ProductCard from "../hot-products/productCard";
+import { dataOptions, IProduct, IOption } from "./home-type-products.interface";
 
 const HomeTypeProducts = () => {
-  const [optionSelected, setOptionSelected] = useState<number>(1)
-  
+  const [optionSelected, setOptionSelected] = useState<IOption>(dataOptions[0]);
+  // optionSelected.value => gaming/ofice/design/student
+
+  const [data, setData] = useState<IProduct[]>(
+    products.filter((x) => x.category === optionSelected.value)
+  );
+
+  console.log('data: ', data);
+
+  // có 2 cách để lọc dữ liệu
+  // cách 1: filter/lọc trực tiếp ở mapping products
+  // cách 2: tạo 1 state data chỉ chứa những product cần filter/lọc
+
   return (
     <div>
       <h2>Sản Phẩm Theo Danh Mục</h2>
@@ -30,13 +25,30 @@ const HomeTypeProducts = () => {
           {dataOptions.map((item) => (
             <div
               key={item.id}
-              onClick={() => setOptionSelected(item.id)}
-              className={`px-4 py-3 text-md font-semibold rounded-full cursor-pointer ${optionSelected === item.id ? "bg-blue-600 text-white" : "text-black bg-white"}`}
+              onClick={() => {
+                setOptionSelected(item);
+                setData(products.filter((x) => x.category === item.value))
+              }}
+              className={`px-4 py-3 text-md font-semibold rounded-full cursor-pointer ${
+                optionSelected.id === item.id
+                  ? "bg-blue-600 text-white"
+                  : "text-black bg-white"
+              }`}
             >
               {item.label}
             </div>
           ))}
         </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 my-8">
+        {products
+          .filter((x) => x.category === optionSelected.value)
+          .map((item: IProduct, index: number) => (
+            <ProductCard key={index} item={item} />
+          ))}
+          {/* {data.map((item: IProduct, index: number) => (
+            <ProductCard key={index} item={item} />
+          ))} */}
       </div>
     </div>
   );
